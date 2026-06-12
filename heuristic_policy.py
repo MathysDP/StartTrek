8194a61b958cd7e6adde8652f2a15d37b903ef61
ThePepidev <mathys1.dupont@epitech.eu>
2026-06-12 13:26:15 +0200

    fix(heuristic_policy): fixing landing velocity issue

--- heuristic_policy.py
+++ heuristic_policy.py
@@ -26,15 +26,15 @@ PREFIX="heuristic_policy"
 
 def policy(obs, env):
     x, y, v_x, v_y, theta, theta_dot, left_leg_contact, right_leg_contact = obs
-    if left_leg_contact == 1 or right_leg_contact == 1:
+    if left_leg_contact == 1 and right_leg_contact == 1:
         return 0
-    if y < 0.7 and v_y < -0.25:
+    if y < 0.7 and v_y < -0.15:
         return 2
     if theta_dot < -0.2 and theta < -0.15:
         return 1
     if theta_dot > 0.2 and theta > 0.15:
         return 3
-    if v_y < -0.5:
+    if v_y < -0.35:
         return 2
     if theta < -0.25:
         return 1
--- main.py
+++ main.py
@@ -2,22 +2,21 @@ import gymnasium as gym
 from gymnasium.wrappers import RecordEpisodeStatistics, RecordVideo
 from heuristic_policy import policy, PREFIX
 
-SEED = 42
-nb_episodes=5
+SEED = 45
+nb_episodes=50
 env = gym.make("LunarLander-v3", render_mode="rgb_array")  # discrete by default
 env = RecordVideo(
     env,
     video_folder="lunarlander-agent",    # Folder to save videos
-    name_prefix=PREFIX,               # Prefix for video filenames
+    name_prefix=PREFIX,             # Prefix for video filenames
     episode_trigger=lambda x: True    # Record every episode
 )
 
 env = RecordEpisodeStatistics(env, buffer_length=nb_episodes)
 
-obs, info = env.reset(seed=SEED)
 i = 0
 for episode in range(nb_episodes):
-	obs, info = env.reset()
+	obs, info = env.reset(seed=SEED + episode)
 	episode_over = False
 	reward = 0
 	while not episode_over:
@@ -27,7 +26,7 @@ for episode in range(nb_episodes):
 		obs, r, terminated, truncated, info = env.step(action)
 		reward += r
 		if terminated or truncated:
-			print(reward)
+			print(f"Episode {episode} : reward {reward}")
 			episode_over = True
 		i += 1
 
