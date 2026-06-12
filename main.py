0b58680a09633e1c77c3898d82ab7a96479db63c
ThePepidev <mathys1.dupont@epitech.eu>
2026-06-12 13:26:19 +0200

    feat(main.py): giving best episode at the end

--- main.py
+++ main.py
@@ -14,23 +14,23 @@ env = RecordVideo(
 
 env = RecordEpisodeStatistics(env, buffer_length=nb_episodes)
 
-i = 0
 mean = 0
+best_episode = tuple()
 for episode in range(nb_episodes):
 	obs, info = env.reset(seed=SEED + episode)
 	episode_over = False
 	reward = 0
 	while not episode_over:
-		if i == 1:
-			print(obs)
 		action = policy(obs, env)
 		obs, r, terminated, truncated, info = env.step(action)
 		reward += r
 		if terminated or truncated:
-			print(f"Episode {episode} : reward {reward}", episode)
+			if best_episode == () or reward > best_episode[1]:
+				best_episode = (episode, reward)
+			print(f"Episode {episode} : reward {reward}")
 			mean += reward
 			episode_over = True
-		i += 1
 
+print("\nbest episode:", best_episode[0], float(best_episode[1]))
 print("Mean reward:", mean / nb_episodes)
 env.close()
